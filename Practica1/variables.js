761fd5c5b7652f5ad9cd1e3b329e8fcaeb4d7bca
grsnpastrana <grsnpastrana@gmail.com>
2025-05-19 17:14:16 -0600

    ejercicio 2

--- Practica1/variables.js
+++ Practica1/variables.js
@@ -1,9 +1,22 @@
- var nombre_1 = "Armando";
+/* var nombre_1 = "Armando";
 var edad = 25;
 let nombre = "Ana Maria";
 var saludo = "Hola, " + nombre + ". Tienes " + edad + "años.";
 
 console.log (saludo)
 
- 
+ */
 
+numero = 25
+function cuadrado(numero){
+    return numero * numero;
+}
+
+console.log(cuadrado(numero))
+
+
+let numero = 25;
+
+const cuadrado = (numero) => numero * numero;
+
+console.log(cuadrado(numero));
