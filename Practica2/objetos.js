c0ccf64d2fb0b597919fb314cddfd8a05e9447fb
grsnpastrana <grsnpastrana@gmail.com>
2025-05-21 19:12:07 -0600

    ejercicio 2

--- Practica2/objetos.js
+++ Practica2/objetos.js
@@ -1,3 +1,4 @@
+/* desestructuracion */
 const persona = {
     nombre : "Ivan Isay",
     edad : 37,
@@ -9,5 +10,16 @@ const persona = {
 
 const {nombre, edad, direccion:{ciudad,pais}} = persona;
 
-const presentar = (nombre, edad,ciudad) => "Me llamo " + nombre + " tengo" + edad + " años y vivo en " + ciudad;
+const presentar = (nombre, edad,ciudad) => "Me llamo " + nombre + " tengo " + edad + " años y vivo en " + ciudad;
 console.log(presentar(nombre,edad,ciudad));
+
+/* funcion map */
+const productos = [
+    {nombre: "Laptop", precio: 12000},
+    {nombre: "Mouse", precio: 250},
+    {nombre: "Teclado", precio: 700},
+    {nombre: "Monitor", precio: 3000},
+];
+
+const nombres = productos.map(productos => productos.nombre)
+console.log(nombres);
