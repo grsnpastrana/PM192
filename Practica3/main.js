b377eb4d5cde38bed2117f1f03fe447de9ee09db
grsnpastrana <grsnpastrana@gmail.com>
2025-05-23 11:46:25 -0600

    ejercicio2

--- Practica3/main.js
+++ Practica3/main.js
@@ -1,3 +1,13 @@
 const restar = require('./utils');
 
 console.log("25 - 18 =", restar(25, 18));
+
+
+const verificarUsuario = require('./promesa');
+verificarUsuario("admin")
+  .then(mensaje => console.log(mensaje))   
+  .catch(error => console.log(error));
+
+verificarUsuario("juan")
+  .then(mensaje => console.log(mensaje))
+  .catch(error => console.log(error));      
---
+++ Practica3/promesa.js
@@ -0,0 +1,11 @@
+function verificarUsuario(usuario){
+    return new Promise((resolve, reject) => {
+    if (usuario === "admin") {
+      resolve("Acceso concedido");
+    } else {
+      reject("Acceso denegado");
+    }
+  });
+}
+
+module.exports = verificarUsuario;
