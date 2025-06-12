2ca23550c3256f03d0808fed215ee57d6d917cbc
grsnpastrana <grsnpastrana@gmail.com>
2025-06-11 18:31:43 -0600

    Practica 5 children

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,21 +1,21 @@
 
 /* zona 1, aqui van importaciones  */
 import { StatusBar } from 'expo-status-bar';
+import { Children } from 'react';
 import { StyleSheet, Text, View,Button } from 'react-native';
 
 const Texto=(props)=>{
-  const{contenido}=props
-  return(<Text>{contenido}</Text>) 
+  const{children}=props
+  return(<Text>{children}</Text>) 
 }
 
 /* zona 2 , el main*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto contenido="hola"></Texto>
-      <Texto contenido="mundo"></Texto>
-      <Texto contenido="React Native"></Texto>
-      <Text>Hola mundo React Native</Text>
+      <Texto> "hola"</Texto>
+      <Texto> "mundo"</Texto>
+      <Texto>"React Native"</Texto>
       <Button title='Presioname!'></Button>
       <StatusBar style="auto" />
     </View>
