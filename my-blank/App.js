67e28bfde44afaf3b33972004d9fe0013e4debdd
grsnpastrana <grsnpastrana@gmail.com>
2025-06-11 18:19:24 -0600

    Practica 5  crear componente y mandar llamar

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+/* zona 1, aqui van importaciones  */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Button } from 'react-native';
+
+const Texto=(props)=>{
+  const{contenido}=props
+  return(<Text>{contenido}</Text>) 
+}
+
+/* zona 2 , el main*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido="hola"></Texto>
+      <Texto contenido="mundo"></Texto>
+      <Texto contenido="React Native"></Texto>
+      <Text>Hola mundo React Native</Text>
+      <Button title='Presioname!'></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+/* zona 3, estilos */ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
