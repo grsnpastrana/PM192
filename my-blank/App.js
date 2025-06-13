1a57a8485b603a823ea6e8f28e1c8d7f927963cf
grsnpastrana <grsnpastrana@gmail.com>
2025-06-12 18:32:58 -0600

    Practica 6 conocer todos los atributos

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,26 +1,23 @@
-
 /* zona 1, aqui van importaciones  */
 import { StatusBar } from 'expo-status-bar';
 import { Children } from 'react';
 import { StyleSheet, Text, View,Button } from 'react-native';
 import React, {useState} from 'react';
 
-const Texto=()=>{
+const Texto=({style})=>{
   const[contenido, setContenido]=useState('hola mundo React')
   const actualizarTexto=()=>{setContenido('estado actualizado')}
-  return(
-  <Text style={styles.Text}onPress={actualizarTexto}>{contenido}</Text>
-) 
+  return(<Text style={[styles.text, style]} onPress={actualizarTexto}>{contenido}</Text>) 
 }
 
 /* zona 2 , el main*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto>  </Texto>
-      <Texto> </Texto>
-      <Texto> </Texto>
-      <Button title='Presioname!'></Button>
+      <Texto style={styles.red}> </Texto>
+      <Texto style={styles.green}> </Texto>
+      <Texto style={styles.blue}> </Texto>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -32,11 +29,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'base-line',
     justifyContent: 'center',
-  }, 
-  Text:{
-    color:'red',
-    fontSize:25,
+    flexDirection:'column'
   },
+
+text:{
+ color:'white',
+ fontSize:25,
+/*  height:100, */
+/*  width:100, */
+
+
+ },
+red:{backgroundColor:'red'},
+green:{backgroundColor:'green'},
+blue:{backgroundColor:'blue'},
+
+
 });
