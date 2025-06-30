6ff08ce4cc748d259f331db473e048d69b477335
grsnpastrana <grsnpastrana@gmail.com>
2025-06-30 15:03:16 -0600

    Practica 12 y 13

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,58 +1,55 @@
-/* Zone 1: Importaciones */
-import { ImageBackground, StyleSheet, Text, TextInput, Button, Alert, View, SafeAreaView } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
 
-
-
-/* Zone 2: Main */
+const IndicadorCarga = ({ color, size }) => {
+  return <ActivityIndicator style={styles.indicator} color={color} size={size} />;
+};
 
 export default function App() {
-  const [nombre, setNombre] = useState('');
+  const [cargando, setCargando] = useState(false);
 
-  const mostrarAlerta=()=>{
-    if (nombre.trim()===''){
-      Alert.alert('error, por favor escribe algo ');
-      alert('Escribe algo')
-    } else {
-      Alert.alert('Bienvenido', `hola ${nombre}, bienvnido a nuestra app`);
-      alert('hola'+ nombre+ 'bienvenido');
-
-    }
-  }
+  const iniciarCarga = () => {
+    setCargando(true);
+    setTimeout(() => {
+      setCargando(false);
+    }, 3000); // le puse 3000 ms para que se vea el indicador
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Ingresa tu nombre</Text>
-      <TextInput style={styles.input}
-      placeholder='Escribe tu nombre'
-      onChangeText={setNombre}
-      value={nombre}
-      ></TextInput>
-
-      <Button title='Enviar'onPress={mostrarAlerta}></Button>
-
+      <Text style={styles.textoPrincipal}>Uso de ActivityIndicator</Text>
+      {cargando ? (
+        <IndicadorCarga color="deepskyblue" size="large" />
+      ) : (
+        <Text style={styles.textoSecundario}>Presiona el botón para comenzar</Text>
+      )}
+      <Button title="Iniciar Carga" onPress={iniciarCarga} color="#ff6f61" />
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-/* Zone 3: Estilos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:'center',
-    padding:20,
-    backgroundColor:'white'
+    backgroundColor: '#ccff90',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
   },
-  text:{
-    fontSize:18,
-    marginBottom:20,
-    color:'#000',
+  textoPrincipal: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 30,
+    color: '#2e2e2e',
   },
-  input:{
-    borderWidth: 1,
-    borderColor:'#ccc',
-    padding: 10,
-    marginBottom:20,
-  }
-
+  textoSecundario: {
+    fontSize: 16,
+    marginVertical: 20,
+    color: '#3a3a3a',
+  },
+  indicator: {
+    marginBottom: 20,
+  },
 });
